d8004bc0733ede85561d0f7e5adbf9f3126954c9
Ikari_Eren <tmm2469938295@163.com>
2023-01-09 15:39:46 +0800

    Update

---
+++ gal/script.js
@@ -0,0 +1,28 @@
+var galNumber = 5 -1; //Gal数量
+function sayhuahuo() {
+    var message = confirm("请遵守网络秩序，注意自己的言行。");
+    var sayhuahuo = document.getElementById("sayhuahuo");
+    if (message == true) {
+        sayhuahuo.onclick = "null";
+        sayhuahuo.innerHTML = "https://www.sayhuahuo.xyz/";
+        sayhuahuo.href = "https://www.sayhuahuo.xyz/";
+    }
+    else if (message == false) {
+        window.close();
+    }
+}
+function torrentTime() {
+    var timeNow = new Date().getHours();
+    var torrentStatus = document.getElementsByName("torrent");
+    if (timeNow >= 9 && timeNow <= 21) {
+        for (var i = 0; i <= galNumber; i++) {
+            torrentStatus[i].innerHTML = "做种中";
+        }
+    }
+    else if (timeNow <= 8 && timeNow >= 22) {
+        for (var i = 0; i <= galNumber; i++) {
+            torrentStatus[i].innerHTML = "未作种";
+        }
+    }
+}
+window.onload = torrentTime();
