83360425f49d52bdf11f6a9e07fbbda7dc3b5897
Ikari_Eren <tmm2469938295@163.com>
2023-01-17 21:43:31 +0800

    Update

--- script.js
+++ script.js
@@ -20,6 +20,54 @@ function torrentTime() {
         torrentStatus.innerHTML = "做种状态：未作种";
     }
 }
+function downloadSet(method) {
+    var url = "download.json";
+    var request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            var jsonObj = JSON.parse(request.responseText);
+            var downloadMethods = document.getElementsByName("download");
+            if (method == 0) {
+                for (var i = 0; i < downloadMethods.length; i++) {
+                    downloadMethods[i].innerHTML = "BT种子";
+                    downloadMethods[i].href = jsonObj.download[i].bt;
+                    downloadMethods[i].className = "button";
+                }
+            }
+            if (method == 1) {
+                for (var i = 0; i < downloadMethods.length; i++) {
+                    downloadMethods[i].innerHTML = "度盘秒传";
+                    downloadMethods[i].href = jsonObj.download[i].baidu;
+                    downloadMethods[i].className = "link";
+                }
+            }
+        }
+    }
+    request.open("get", url, true);
+    request.send();
+}
 $(document).ready(function () {
     torrentTime();
+    $("#downloadMethod1").removeClass("button");
+    $("#downloadMethod1").addClass("buttonDisabled");
+    downloadSet(0);
+    $("#downloadMethod1").click(function () {
+        $(this).addClass("buttonDisabled");
+        $(this).removeClass("button");
+        $("#downloadMethod2").removeClass("buttonDisabled");
+        $("#downloadMethod2").addClass("button");
+        downloadSet(0);
+    });
+    $("#downloadMethod2").click(function () {
+        $(this).addClass("buttonDisabled");
+        $(this).removeClass("button");
+        $("#downloadMethod1").removeClass("buttonDisabled");
+        $("#downloadMethod1").addClass("button");
+        downloadSet(1);
+    });
 });
